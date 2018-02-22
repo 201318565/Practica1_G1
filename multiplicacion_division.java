2b4ca6c8268566d5269a5a9c0e0ab7b2e8d792b1
Diego Fuentes <triforceoftime@gmail.com>
2018-02-22 15:33:59 -0600

    Arreglado dividir entre 0

--- multiplicacion_division.java
+++ multiplicacion_division.java
@@ -2,6 +2,13 @@ public float multiplicacion(float num1, float num2){
 	return num1 * num2;
 }
 public float division(float num1, float num2){
-	return num1 / num2;
+	if(num2==0){
+		if(num1==0)
+			return "Error";
+		else
+		return "Infinity";
+	}
+	else
+		return num1 / num2;
 }
 
