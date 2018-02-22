f2a5d225d1e0906b81f1e40dfb568eebc25433a6
luiscarrillo129@gmail.com <luiscarrillo129@gmail.com>
2018-02-21 21:26:33 -0600

    funcionalidad

--- potencia_cubo.java
+++ potencia_cubo.java
@@ -1,2 +1,7 @@
-
+public float potencia_al_cubo(float num){
+	return num * num;
+}
+public float raiz_cubica(float num){
+	return java.lang.Math.sqrt(num);
+}
 
--- potencia_raiz.java
+++ potencia_raiz.java
@@ -1,2 +1,6 @@
 public float potencia_al_cuadrado(float num){
-
+	return num * num;
+}
+public float raiz_cuadrada(float num){
+	return java.lang.Math.sqrt(num);
+}
